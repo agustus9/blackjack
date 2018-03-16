73a906ffdb83310522c5bb3b705a37312034ab2b
Mack <amack@proforma.com>
2018-03-16 13:03:58 -0400

    Added Shuffle deck

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
+// Create Cards
 const Card = function (value, name, suit) {
   this.value = value;
   this.name = name;
   this.suit = suit;
 }
-
+// Create Deck
 const Deck = function () {
   this.names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
   this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
@@ -16,16 +17,16 @@ const Deck = function () {
   }
 
   console.log(this);
-
+  // Card range 
   const randomNum = function (min, max) {
-    return Math.floor(Math.random() * (max - min ));
+    return Math.floor(Math.random() * (max - min));
     console.log(min);
   }
-
+  // Shuffle the deck
   const Shuffle = function (deck) {
     temp = [];
-    for(i = 0; i < deck.length; i++) {
-      
+    for (i = 0; i < deck.length; i++) {
+
     }
   }
 }

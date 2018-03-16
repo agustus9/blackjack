dfdc273602ff8587d10cc547e081a2b9e1243966
Mack <amack@proforma.com>
2018-03-16 09:50:13 -0400

    Finished Deck

--- public/main.js
+++ public/main.js
@@ -1,5 +1,26 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const Card = function (value, name, suit) {
+  this.value = value;
+  this.name = name;
+  this.suit = suit;
 }
+
+const Deck = function () {
+  this.names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+  this.cards = new Array();
+
+  for (let s = 0; s < this.suits.length; s++) {
+    for (let n = 0; n < this.names.length; n++) {
+      this.cards.push(new Card(n + 1, this.names[n], this.suits[s]));
+    }
+  }
+
+  console.log(this);
+}
+
+const main = () => {
+  deck = new Deck();
+}
+
 
 document.addEventListener('DOMContentLoaded', main)
